3cd9d299b0fce096635a32c0ad6bee0a7b387663
Anupam Sharma <mr.anupamkrsharma@gmail.com>
2021-11-16 09:41:19 +0530

    update commit
    ---------- ** fixes ** ----------
    
    >>  mute toggle  working on next or previous btn clicked
    
    >> repeat toggle working on next or previous btn clicked
    
    ---------- ** bugs ** ----------
    
    >> song cann't update when clicked or drag the seekbar

--- js/main.js
+++ js/main.js
@@ -1,26 +1,33 @@
-const audio_info = `[
+// audio lists
+const list_1 = document.getElementById('1')
+const list_2 = document.getElementById('2')
+const list_3 = document.getElementById('3')
+const list_4 = document.getElementById('4')
+
+const audio = [
     {
-        "name": "Batchbug",
-        "artist": "Sweet Dreams"
+        name: "Batchbug",
+        artist: "Sweet Dreams",
+        scr: list_1
     },
     {
-        "name": "Cali",
-        "artist": "Beats"
+        name: "Cali",
+        artist: "Beats",
+        scr: list_2
     },
     {
-        "name": "Chillout Lounge",
-        "artist": "Slow Grind"
+        name: "Chillout Lounge",
+        artist: "Slow Grind",
+        scr: list_3
     },
     {
-        "name": "Dreaming Under The Stars",
-        "artist": "Romance"
+        name: "Dreaming Under The Stars",
+        artist: "Romance",
+        scr: list_4
     }
-]`
+]
 
-// jSON object
-const obj = JSON.parse(audio_info)
-
-const status = document.getElementById('status')
+const statusText = document.getElementById('status')
 const playlist_icon = document.getElementById('playlist-icon')
 const cross_icon = document.getElementById('cross-icon')
 const modal = document.getElementById('modal')
@@ -30,6 +37,8 @@ const disc = document.getElementById('disc')
 const song_name = document.getElementById('song_name')
 const artist_name = document.getElementById('artist_name')
 
+
+
 // buttons
 const repeatbtn = document.getElementById('repeat')
 const previousbtn = document.getElementById('previous')
@@ -37,16 +46,6 @@ const playbtn = document.getElementById('play')
 const nextbtn = document.getElementById('next')
 const volumebtn = document.getElementById('volume')
 
-// audio lists
-const list_1 = document.getElementById('1')
-const list_2 = document.getElementById('2')
-const list_3 = document.getElementById('3')
-const list_4 = document.getElementById('4')
-
-// audio list array and index for starting index and isplaying is for flag
-const playlist = [list_1, list_2, list_3,list_4]
-let index = 0
-let isplaying = true
 
 // seek bar
 const end_duration = document.getElementById('end_duration')
@@ -58,181 +57,180 @@ const range = document.getElementById('range')
 const dot = document.getElementById('dot')
 
 
+// flags containing for the check status
+let isplaying = false
+let ismuted = false
+let isrepeated = false
+let index = 0
 
-// update the seekbar with current time
-function updateSlider(currentTime, duration){
-    if(duration){
+
+// update the seekbar with current time.
+function updateSlider(currentTime, duration) {
+    if (duration) {
         let silder = Math.floor((currentTime * 100) / duration)
-        range.style.width = silder + 2  + "%"
-    }  
+        range.style.width = silder + 2 + "%"
+    }
 }
 
 
 
 // track function which track the current time and duration of the playing song
-// and update the time label i.e start duration and end duration
-function trackDuration(e){
-    var {currentTime, duration} = e.target
-  
+// and update the time label i.e start duration and end duration.
+function trackDuration(e) {
+    var { currentTime, duration } = e.target
+
     let currentTime_min = Math.floor(currentTime / 60)
     let currentTime_sec = Math.floor(currentTime % 60)
-            
+
     let duration_min = Math.floor(duration / 60)
     let duration_sec = Math.floor(duration % 60)
-            
-    if(currentTime_sec < 10)
+
+    if (currentTime_sec < 10)
         currentTime_sec = `0${currentTime_sec}`
-      
+
     start_duration.innerHTML = `${currentTime_min}:${currentTime_sec}`
-            
-    if(duration && duration_sec < 10)
+
+    if (duration && duration_sec < 10)
         end_duration.textContent = `${duration_min}:0${duration_sec}`
-    
-    else if(duration)
+
+    else if (duration)
         end_duration.textContent = `${duration_min}:${duration_sec}`
-    
-    updateSlider(currentTime, duration)    
+
+    // update the seekbar length according to time that song has played.    
+    updateSlider(currentTime, duration)
 }
 
 
 
 // plays and stop method
-function plays(){
-    if(isplaying){
-        playlist[index].load()
-        song_name.innerText = obj[index].name
-        artist_name.innerText = obj[index].artist
-        status.innerText = 'Now Playing!'
-        playlist[index].play()
+function songplay() {
+    if (!isplaying) {
+
+        // check if volume btn is muted for the current song or not.
+        audio[index].scr.muted = (ismuted)? true : false
+
+        // check if the repeat btn is active for the current song or not.
+        audio[index].scr.loop = (isrepeated)? true : false
+
+        // plays the current song.
+        audio[index].scr.play()
+        isplaying = true
+
+        song_name.innerText = audio[index].name
+        artist_name.innerText = audio[index].artist
+        statusText.innerText = 'Now Playing!'
         disc.classList.add('spin')
         playbtn.classList.add('active')
-        document.getElementById('play-icon').style.display = "none" 
-        document.getElementById('pause-icon').style.display = "block" 
-        isplaying = false
-        playlist[index].addEventListener('timeupdate',trackDuration)
-    }
-    else{
-        playlist[index].pause()
-        disc.classList.remove('spin')
-        playbtn.classList.remove('active')
-        status.innerText = "Paused!"
-        document.getElementById('play-icon').style.display = "block" 
-        document.getElementById('pause-icon').style.display = "none" 
-        isplaying = true
-    }
-}
+        document.getElementById('play-icon').style.display = "none"
+        document.getElementById('pause-icon').style.display = "block"
 
+        audio[index].scr.addEventListener('timeupdate', trackDuration)
+    }
+    else {
+        // stop the current song and DOM elements
+        stopSong()
+    }
+
+}
 
 
 // this method trigger when the song is finished playing and to reset the Ui
-function stop(){
-    playlist[index].pause()
+function stopSong() {
+
+    //stop the current song
+    audio[index].scr.pause()
+    isplaying = false
+
     disc.classList.remove('spin')
     playbtn.classList.remove('active')
+    statusText.innerText = "Paused!"
     range.style.width = 2 + "%"
     start_duration.innerText = "0:00"
-    document.getElementById('play-icon').style.display = "block" 
-    document.getElementById('pause-icon').style.display = "none" 
-    isplaying = true
+    document.getElementById('play-icon').style.display = "block"
+    document.getElementById('pause-icon').style.display = "none"
 }
 
 
-
-// repeat button handler handles the current song will repeat continuously
-repeatbtn.addEventListener("click", ()=>{
-    if(!repeatbtn.classList.contains('active')){
-        repeatbtn.classList.add('active')
-        playlist[index].loop = true
-    }
-    else{
-        repeatbtn.classList.remove('active')
-        playlist[index].loop = false
-    }
-})
-
-
-
 // play button handler handles the plays() method that is use to play and stop the song
-playbtn.addEventListener("click", plays)
-
-
+playbtn.addEventListener('click', songplay)
 
 // audio handler handles the stop() method when song is finished
-playlist[index].addEventListener("ended",stop)
+audio[index].scr.addEventListener("ended", stopSong)
 
-
-
-// next button handler handles the next song will play when clicked
-nextbtn.addEventListener("click",()=>{
-    if(index < playlist.length-1){
-        playlist[index].pause()
-        status.innerHTML = "Loading..."
-        disc.classList.remove('spin')
-        isplaying = true
+nextbtn.addEventListener('click', () => {
+    if (index < audio.length - 1) {
+        stopSong()
         index++
-        setTimeout(plays, 200);
+        audio[index].scr.load()
+        songplay()
     }
-    else{
-        playlist[index].pause()
-        status.innerHTML = "Loading..."
-        disc.classList.remove('spin')
-        isplaying = true
+    else {
+        stopSong()
         index = 0
-        setTimeout(plays, 200);   
+        audio[index].scr.load()
+        songplay()
     }
 })
 
-
-
-//previous button handler handles the previous song will play when clicked
-previousbtn.addEventListener("click",()=>{
-    if(index > 0){
-        playlist[index].pause()
-        status.innerHTML = "Loading..."
-        disc.classList.remove('spin')
-        isplaying = true
+previousbtn.addEventListener('click', () => {
+    if (index > 0) {
+        stopSong()
         index--
-        setTimeout(plays, 200);
+        audio[index].scr.load()
+        songplay()
     }
-    else{
-        playlist[index].pause()
-        status.innerHTML = "Loading..."
-        disc.classList.remove('spin')
-        isplaying = true
-        index = playlist.length-1
-        setTimeout(plays, 200);
+    else {
+        stopSong()
+        index = audio.length - 1
+        audio[index].scr.load()
+        songplay()
     }
 })
 
+volumebtn.addEventListener('click', () => {
+    if (!ismuted) {
+        ismuted = true
+        audio[index].scr.muted = ismuted
 
-
-// volume button handler handles the mute and unmute state of volume when pressed
-volumebtn.addEventListener("click", ()=>{
-    if(!volumebtn.classList.contains('active')){
         volumebtn.classList.add('active')
-        playlist[index].muted = true
-        document.getElementById('mute').style.display = "block" 
-        document.getElementById('unmute').style.display = "none" 
+        document.getElementById('mute').style.display = "block"
+        document.getElementById('unmute').style.display = "none"
     }
-    else{
+    else {
+        ismuted = false
+        audio[index].scr.muted = ismuted
+
         volumebtn.classList.remove('active')
-        playlist[index].muted = false
-        document.getElementById('unmute').style.display = "block" 
-        document.getElementById('mute').style.display = "none" 
+        document.getElementById('unmute').style.display = "block"
+        document.getElementById('mute').style.display = "none"
+    }
+})
+
+repeatbtn.addEventListener('click', () => {
+    if (!isrepeated) {
+        isrepeated = true
+        audio[index].scr.loop = isrepeated
+        repeatbtn.classList.add('active')
+    }
+    else {
+        isrepeated = false
+        audio[index].scr.loop = isrepeated
+        repeatbtn.classList.remove('active')
     }
 })
 
 
 
 // open playlist handler
-playlist_icon.addEventListener("click",()=>{
-    modal.style.display = "block" 
+playlist_icon.addEventListener("click", () => {
+    modal.style.display = "block"
 })
 
 // close playlsit handler
-cross_icon.addEventListener("click",()=>{
-    modal.style.display = "none" 
+cross_icon.addEventListener("click", () => {
+    modal.style.display = "none"
 })
+
 
 //seekbar handler
 seekbar_slider.addEventListener("mouseover", ()=>{
